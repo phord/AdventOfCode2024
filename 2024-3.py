3a4af8283f0f9d3c9ed986c0860f0feb0a29f853
Phil Hord <phil.hord@gmail.com>
2024-12-02 22:17:37 -0800

    Simplify by removing specific token matcher
    May need to add this back in some future challenge, though.

--- 2024-3.py
+++ 2024-3.py
@@ -2,30 +2,6 @@
 
 ''' AOC 2024, day 3 '''
 
-def decode_token(prog):
-    '''
-    Extracts the next token at the start of string and returns the
-    token and the remainder of the string
-    Recognized tokens:
-        mul
-        (
-        integer
-        ,
-        )
-    Anything else is garbage.  It returns None for the token, and the remainder of the string.
-    '''
-
-    keywords = ["mul", "(", ",", ")", "don't", "do"]
-
-    token, remainder = decode_int(prog)
-    if token is not None:
-        return token, remainder
-
-    for k in keywords:
-        if prog.startswith(k):
-            return k, prog[len(k):]
-    return None, prog[1:]
-
 def is_integer(prog):
     '''
     Returns true if the first character of prog is a digit
@@ -41,15 +17,16 @@ def decode_patt(prog, match):
 
     remainder = prog
     for m in match:
-        token, remainder = decode_token(remainder)
-        catch.append(token)
-        if token is None:
-            return None, prog
         if m == "integer":
-            if not is_integer(token):
-                return None, prog
-        elif token != m:
-            return None, prog
+            if not is_integer(remainder):
+                return (None, prog)
+            token, remainder = decode_int(remainder)
+            assert token is not None
+            catch.append(token)
+        elif remainder.startswith(m):
+            remainder = remainder[len(m):]
+        else:
+            return (None, prog)
     return (catch, remainder)
 
 
@@ -58,24 +35,19 @@ def decode_mul(prog):
     Returns the result of the mul operation and the remainder of the string.
     If the string does not start with a mul operation, returns None, prog.
     '''
-    match = ["mul", "(", "integer", ",", "integer", ")"]
+    match = ["mul(", "integer", ",", "integer", ")"]
 
     catch, remainder = decode_patt(prog, match)
     if catch is None:
         return None, prog
-    return (int(catch[2]) * int(catch[4]), remainder)
+    return (int(catch[0]) * int(catch[1]), remainder)
 
 def decode_do(prog):
     '''
     Returns the result of the do operation and the remainder of the string.
     If the string does not start with a do operation, returns None, prog.
     '''
-    match = ["do", "(", ")"]
-
-    catch, remainder = decode_patt(prog, match)
-    if catch is None:
-        return None, prog
-    return (True, remainder)
+    return decode_patt(prog, ["do()"])
 
 
 def decode_dont(prog):
@@ -83,13 +55,7 @@ def decode_dont(prog):
     Returns the result of the don't operation and the remainder of the string.
     If the string does not start with a don't operation, returns None, prog.
     '''
-    match = ["don't", "(", ")"]
-
-    catch, remainder = decode_patt(prog, match)
-
-    if catch is None:
-        return None, prog
-    return (True, remainder)
+    return decode_patt(prog, ["don't()"])
 
 
 def decode_int(prog):
@@ -141,12 +107,12 @@ def part2(inp):
 
     print(f"Part2: {total}")
 
-input1= "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+part1( "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
 with open('input3.txt', 'r') as f:
     input1 = f.read()
 part1(input1)
 
-input2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+part2( "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))" )
 with open('input3.txt', 'r') as f:
     input2 = f.read()
 part2(input2)
