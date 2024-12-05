f623bdb2ed24d4b40c980ecf3e43ebbae3c8fd94
Phil Hord <phil.hord@gmail.com>
2024-12-05 14:52:06 -0800

    Day 5 cleanup
    Simplified parsed, checkers. Combined Part1/Part2.

--- 2024-5.py
+++ 2024-5.py
@@ -8,76 +8,54 @@ Order rules is a map of pages and their required predecessors.
 Updates is a list of pages for each update to be printed.
 '''
 def parse(inp):
-    a,b = inp.split('\n\n')
-    a = a.split('\n')
-    b = b.split('\n')
+    rules,updates = (x.split('\n') for x in inp.split('\n\n'))
 
     order = {}
-    for p1, p2 in [(int(x), int(y)) for x,y in [line.split('|') for line in a]]:
+    edges = [(int(x), int(y)) for x,y in [line.split('|') for line in rules]]
+    # graph = { key: set(y for x,y in edges if x == key) for key in set(x for x,y in edges)}
+    for p1, p2 in edges:
         if p2 in order:
             order[p2].add(p1)
         else:
             order[p2] = set([p1])
 
-    updates = []
-    for pp in b:
-        updates.append([int(x) for x in pp.split(',')])
+    updates = [[int(x) for x in pp.split(',')] for pp in updates]
 
     return order, updates
-
-
-def valid(pp, order):
-    done = set()
-    # reverse the pages
-    pp = pp[::-1]
-    for p in pp:
-        if p in done:
-            return False
-        if p in order:
-            for inv in order[p]:
-                if inv in done:
-                    return False
-        done.add(p)
-    return True
-
-def part1(order, updates):
-    total = 0
-    for pages in updates:
-        if valid(pages, order):
-            total += pages[len(pages)//2 ]
-    print("Part1: ", total)
 
 def try_fix(pp, order):
     todo = set(pp)
     fixed = []
     for p in pp:
-        if p in fixed:
-            continue
-        if p in order:
-            for inv in order[p]:
-                if inv in todo:
-                    fixed.append(inv)
-        fixed.append(p)
-        todo -= set(fixed)
+        if p in todo:
+            precedents = order[p] if p in order else set()
+            fixed.extend(list(precedents.intersection(todo)))
+            todo -= precedents
+            fixed.append(p)
+            todo.remove(p)
     return fixed
 
 def fix(pp, order):
     fixed = pp
-    while not valid(fixed, order):
-        fixed = try_fix(fixed, order)
+    while True:
+        sorted = try_fix(fixed, order)
+        if fixed == sorted:
+            break
+        fixed = sorted
     return fixed
 
-def part2(order, updates):
-    total = 0
+def solve(order, updates):
+    part1 = 0
+    part2 = 0
     for pages in updates:
-        if not valid(pages, order):
-            fixed = fix(pages, order)
-            assert set(fixed) == set(pages)
-            assert(len(fixed) & 1 == 1)
-            assert(valid(fixed, order))
-            total += fixed[len(fixed)//2 ]
+        sorted = fix(pages, order)
+        if sorted == pages:
+            part1 += pages[len(pages)//2]
+        else:
+            part2 += sorted[len(sorted)//2]
 
-    print("Part2: ", total)
+    print("Part1: ", part1)
+    print("Part2: ", part2)
 
 def sample():
     return '''47|53
@@ -115,19 +93,12 @@ def play():
     # print(updates)
 
 
-    part1( order, updates )
-
-    part2( order, updates )
+    solve( order, updates )
 
     with open('input5.txt', 'r') as f:
         input1 = f.read()
     order, updates = parse(input1)
 
-    part1(order, updates)
-
-    with open('input5.txt', 'r') as f:
-        input2 = f.read()
-    order, updates = parse(input2)
-    part2( order, updates )
+    solve(order, updates)
 
 play()
