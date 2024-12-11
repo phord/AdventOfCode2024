c9f179be940e1a59a7ec2f081e323d96d50a1c31
Phil Hord <phil.hord@gmail.com>
2024-12-11 12:04:18 -0800

    Day11 Completely different approach, 2x as fast
    No memoization needed

--- src/day11.rs
+++ src/day11.rs
@@ -1,5 +1,4 @@
-use std::collections::{HashMap, HashSet};
-
+use std::collections::HashMap;
 
 type Stones = Vec<u64>;
 
@@ -9,53 +8,62 @@ fn input_generator(input: &str) -> Stones {
 }
 
 
-#[derive(Default)]
-struct Blinker {
-    memo: HashMap<(u64, u8), u64>,
+struct StonePopulation {
+    // How many stones of each number do we have?
+    stones: HashMap<u64, u64>,
 }
 
-impl Blinker {
+impl StonePopulation {
+    fn new(inp: &Stones) -> StonePopulation {
+        let mut stones = HashMap::new();
+        for s in inp.iter() {
+            *stones.entry(*s).or_insert(0u64) += 1u64;
+        }
+        StonePopulation { stones }
+    }
 
-    fn blink(&mut self, stone: u64, blinks: u8) -> u64 {
-        if blinks == 0 {
-            1
-        } else if let Some(ans) = self.memo.get(&(stone, blinks)) {
-            *ans
-        } else {
-            let ans = if stone == 0 {
-                self.blink(1, blinks - 1)
+    fn blink(&mut self) {
+        let mut new_stones = HashMap::new();
+        for (stone, count) in self.stones.iter() {
+            if stone == &0 {
+                *new_stones.entry(1).or_insert(0) += count;
             } else {
                 let log = stone.checked_ilog10().unwrap_or(0) + 1;
                 if log % 2 == 0 {
                     let div = 10_u64.pow(log / 2);
-                    self.blink(stone / div, blinks -1) + self.blink(stone % div, blinks - 1)
+                    *new_stones.entry(stone / div).or_insert(0) += count;
+                    *new_stones.entry(stone % div).or_insert(0) += count;
                 } else {
-                    self.blink(stone * 2024, blinks - 1)
+                    *new_stones.entry(stone * 2024).or_insert(0) += count;
                 }
             };
-            self.memo.insert((stone, blinks), ans);
-            ans
         }
+        self.stones = new_stones;
     }
 
-
-    fn solve(&mut self, stones: &Stones, blinks: u8) -> u64 {
-        stones.iter().map(|s| self.blink(*s, blinks)).sum()
+    fn len(&self) -> u64 {
+        self.stones.values().sum()
     }
+
 }
 
 
+fn solve(stones : &Stones, blinks: u8) -> u64 {
+    let mut stones = StonePopulation::new(stones);
+    for _ in 0..blinks {
+        stones.blink();
+    }
+    stones.len()
+}
+
 #[aoc(day11, part1)]
 fn part1(stones : &Stones) -> u64 {
-    println!("{:?}", &stones);
-    let mut blink = Blinker::default();
-    blink.solve(stones, 25)
+    solve(stones, 25)
 }
 
 #[aoc(day11, part2)]
 fn part2(stones : &Stones) -> u64 {
-    let mut blink = Blinker::default();
-    blink.solve(stones, 75)
+    solve(stones, 75)
 }
 
 
@@ -63,14 +71,12 @@ fn part2(stones : &Stones) -> u64 {
 mod tests {
     use super::*;
 
-    // const SAMPLE: &str = "0 1 10 99 999";
     const SAMPLE: &str = "125 17";
 
     #[test]
     fn sample1() {
-        let mut blink = Blinker::default();
-        assert_eq!(blink.solve(&input_generator(SAMPLE), 5), 13);
-        assert_eq!(blink.solve(&input_generator(SAMPLE), 6), 22);
+        assert_eq!(solve(&input_generator(SAMPLE), 5), 13);
+        assert_eq!(solve(&input_generator(SAMPLE), 6), 22);
         assert_eq!(part1(&input_generator(SAMPLE)), 55312);
         assert_eq!(part2(&input_generator(SAMPLE)), 65601038650482);
     }
@@ -78,8 +84,13 @@ mod tests {
 
     #[test]
     fn sample2() {
-        let mut blink = Blinker::default();
-        assert_eq!(blink.solve(&input_generator(SAMPLE), 75), 65601038650482);
+        assert_eq!(solve(&input_generator(SAMPLE), 75), 65601038650482);
+    }
+
+    #[test]
+    fn sample3() {
+        let stones = input_generator(SAMPLE);
+        assert_eq!(solve(&stones, 75), 65601038650482);
     }
 
 }
