82a5c621856bb4e7a6311c863146fbf4de3b01d8
Phil Hord <phil.hord@gmail.com>
2024-12-11 00:13:02 -0800

    Way too slow

--- src/day11.rs
+++ src/day11.rs
@@ -58,12 +58,14 @@ impl Blink {
     }
 
     fn memoize(&mut self, stone: u64, blinks: u8, stones: &Stones) {
-        self.memo.insert((stone, blinks), stones.clone());
-        let exists = self.mapped.get_mut(&stone);
-        if let Some(bar) = exists {
-            bar.insert(blinks);
-        } else {
-            self.mapped.insert(stone, HashSet::from([blinks]));
+        if blinks > 10 {
+            self.memo.insert((stone, blinks), stones.clone());
+            let exists = self.mapped.get_mut(&stone);
+            if let Some(bar) = exists {
+                bar.insert(blinks);
+            } else {
+                self.mapped.insert(stone, HashSet::from([blinks]));
+            }
         }
     }
 
@@ -77,14 +79,23 @@ impl Blink {
                     if blinks == 0 {
                         // No more blinks for this stone
                         stones.push(stone);
+                        // print!("=");
                     } else if let Some(prev) = self.prev(stone, blinks) {
                         // Found some partial work we previously did
+                        println!("   HIT!!! ");
                         work.extend(prev);
+                    } else if blinks > 1 {
+                        work.extend(
+                            self.blink(stone, blinks - 1).iter()
+                                .map(|s| (*s, 1))
+                        );
                     } else
                     // Blink at this stone
                     if stone == 0 {
+                        // print!(">");
                         work.push((1, blinks - 1));
                     } else {
+                        // print!("O");
                         let blinks = blinks - 1;
                         let log = stone.checked_ilog10().unwrap_or(0) + 1;
                         if log % 2 == 0 {
@@ -98,12 +109,26 @@ impl Blink {
                 }
                 None => break,
             }
+            // print!("+");
+
         }
 
         // Record this work
-        self.memoize(stone, blinks, &stones);
+        if blinks > 5 || stones.len() > 100 {
+            println!("{}  {}  {}", stone, blinks, stones.len());
+            self.memoize(stone, blinks, &stones);
+        }
 
         stones
+    }
+
+    fn solve(&mut self, stones: &Stones, blinks: u8) -> usize {
+        let mut result = Stones::default();
+        for stone in stones.iter() {
+            result.extend(self.blink(*stone, blinks));
+            // println!("{:?}", stones);
+        }
+        result.len()
     }
 
 }
@@ -116,13 +141,25 @@ fn blink(stones: &Stones) -> Stones {
 
 
 
-fn solve(stones: &Stones, blinks: u32) -> usize {
-    let mut stones = stones.clone();
-    for _ in 0..blinks {
-        stones = blink(&stones);
+fn solve(stones: &Stones, blinks: u8) -> usize {
+    let mut blink = Blink::default();
+    let mut result = Stones::default();
+    for stone in stones.iter() {
+        result.extend(blink.blink(*stone, blinks));
         // println!("{:?}", stones);
     }
-    stones.len()
+    result.len()
+}
+
+fn solve2(stones : &Stones, blinks: u8) -> usize {
+    let mut blink = Blink::default();
+    for s in 0..blinks/5 {
+
+        let ans = blink.solve(stones, s * 5);
+        println!("{}: {}", s * 5, ans);
+    }
+    // blink.solve(stones, 75)
+    blink.solve(stones, blinks)
 }
 
 #[aoc(day11, part1)]
@@ -133,7 +170,7 @@ fn part1(stones : &Stones) -> usize {
 
 #[aoc(day11, part2)]
 fn part2(stones : &Stones) -> usize {
-    solve(stones, 75)
+    solve2(stones, 75)
 }
 
 
@@ -148,6 +185,14 @@ mod tests {
     fn sample1() {
         assert_eq!(solve(&input_generator(SAMPLE), 6), 22);
         assert_eq!(part1(&input_generator(SAMPLE)), 55312);
+        // assert_eq!(part2(&input_generator(SAMPLE)), 1928);
+    }
+
+
+    #[test]
+    fn sample2() {
+        assert_eq!(solve2(&input_generator(SAMPLE), 6), 22);
+        assert_eq!(solve2(&input_generator(SAMPLE), 25), 55312);
         // assert_eq!(part2(&input_generator(SAMPLE)), 1928);
     }
 
