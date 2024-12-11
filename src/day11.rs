abfc2303328f24d7abc91b4efb42315c31028da9
Phil Hord <phil.hord@gmail.com>
2024-12-11 11:36:08 -0800

    Day11 - finally works, but it still could be faster.
    Without memoization, it's massively slower.
    
    Day 11 - Part 1 : 231278
            generator: 3.455µs,
            runner: 379.614µs
    
    Day 11 - Part 2 : 274229228071551
            generator: 1.306µs,
            runner: 21.883895ms

--- src/day11.rs
+++ src/day11.rs
@@ -9,156 +9,53 @@ fn input_generator(input: &str) -> Stones {
 }
 
 
-
-/*
-If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
-If the stone is engraved with a number that has an even number of digits, it is replaced by two stones. The left half of the digits are engraved on the new left stone, and the right half of the digits are engraved on the new right stone. (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
-If none of the other rules apply, the stone is replaced by a new stone; the old stone's number multiplied by 2024 is engraved on the new stone.
- */
-
-fn blink_one(stone: u64) -> Stones {
-    if stone == 0 { vec![1] }
-    else {
-        let log = stone.to_string().len() as u32;
-        // if stone < 10 { 1} else { (stone - 1).ilog10() + 1};
-        if log % 2 == 0 {
-            let div = 10_u64.pow(log / 2);
-            vec![stone / div, stone % div]
-        } else {
-            vec![stone * 2024]
-        }
-    }
-}
-
-enum Work {
-    Blink(u64, u8),
-    Done(u64)
-}
-
-type Memo = HashMap<(u64, u8), usize>;
-type Mapped = HashMap<u64, HashSet<u8>>;
 #[derive(Default)]
-struct Blink {
-     memo: Memo,
-     mapped: Mapped,
+struct Blinker {
+    memo: HashMap<(u64, u8), u64>,
 }
 
-impl Blink {
+impl Blinker {
 
-    fn prev(&self, stone: u64, blinks: u8) -> Option<usize> {
-        self.memo.get(&(stone, blinks)).copied()
-    }
-
-    fn memoize(&mut self, stone: u64, blinks: u8, stones: usize) {
-        if blinks > 10 {
-            self.memo.insert((stone, blinks), stones);
-            let exists = self.mapped.get_mut(&stone);
-            if let Some(bar) = exists {
-                bar.insert(blinks);
+    fn blink(&mut self, stone: u64, blinks: u8) -> u64 {
+        if blinks == 0 {
+            1
+        } else if let Some(ans) = self.memo.get(&(stone, blinks)) {
+            *ans
+        } else {
+            let ans = if stone == 0 {
+                self.blink(1, blinks - 1)
             } else {
-                self.mapped.insert(stone, HashSet::from([blinks]));
-            }
-        }
-    }
-
-    fn blink(&mut self, stone: u64, blinks: u8) -> usize {
-        let mut work = vec![(stone, blinks)];
-        let mut stones = 0;
-
-        while !work.is_empty() {
-            match work.pop() {
-                Some((stone, blinks)) => {
-                    if blinks == 0 {
-                        // No more blinks for this stone
-                        stones += 1;
-                        // print!("=");
-                    } else if let Some(prev) = self.prev(stone, blinks) {
-                        // We've seen this one before
-                        // println!("   HIT!!! {} {} => {}", stone, blinks, prev);
-                        stones += prev;
-                    // } else if blinks > 1 {
-                    //     work.extend(
-                    //         self.blink(stone, blinks - 1).iter()
-                    //             .map(|s| (*s, 1))
-                    //     );
-                    } else
-                    // Blink at this stone
-                    if stone == 0 {
-                        // print!(">");
-                        work.push((1, blinks - 1));
-                    } else {
-                        // print!("O");
-                        let blinks = blinks - 1;
-                        let log = stone.checked_ilog10().unwrap_or(0) + 1;
-                        if log % 2 == 0 {
-                            let div = 10_u64.pow(log / 2);
-                            work.push((stone / div, blinks));
-                            work.push((stone % div, blinks));
-                        } else {
-                            work.push((stone * 2024, blinks));
-                        }
-                    }
+                let log = stone.checked_ilog10().unwrap_or(0) + 1;
+                if log % 2 == 0 {
+                    let div = 10_u64.pow(log / 2);
+                    self.blink(stone / div, blinks -1) + self.blink(stone % div, blinks - 1)
+                } else {
+                    self.blink(stone * 2024, blinks - 1)
                 }
-                None => break,
-            }
-            // print!("+");
-
+            };
+            self.memo.insert((stone, blinks), ans);
+            ans
         }
-
-        // Record this work
-        if blinks > 5 || stones > 100 {
-            // println!("Memoize: {}  {}  {}", stone, blinks, stones);
-            self.memoize(stone, blinks, stones);
-        }
-
-        stones
     }
 
-    fn solve(&mut self, stones: &Stones, blinks: u8) -> usize {
-        let mut count = 0;
-        for stone in stones.iter() {
-            count += self.blink(*stone, blinks);
-            // println!("{:?} {} ==> {}", stones, blinks, count);
-        }
-        count
+
+    fn solve(&mut self, stones: &Stones, blinks: u8) -> u64 {
+        stones.iter().map(|s| self.blink(*s, blinks)).sum()
     }
-
 }
 
-fn blink(stones: &Stones) -> Stones {
-    stones.iter().flat_map(|s|
-        blink_one(*s)
-    ).collect()
-}
-
-
-
-fn solve(stones: &Stones, blinks: u8) -> usize {
-    let mut blink = Blink::default();
-    let mut result = Stones::default();
-    blink.solve(stones, blinks)
-}
-
-fn solve2(stones : &Stones, blinks: u8) -> usize {
-    let mut blink = Blink::default();
-    for s in 0..blinks/5 {
-
-        let ans = blink.solve(stones, s * 5);
-        println!("{}: {}", s * 5, ans);
-    }
-    // blink.solve(stones, 75)
-    blink.solve(stones, blinks)
-}
 
 #[aoc(day11, part1)]
-fn part1(stones : &Stones) -> usize {
+fn part1(stones : &Stones) -> u64 {
     println!("{:?}", &stones);
-    solve(stones, 25)
+    let mut blink = Blinker::default();
+    blink.solve(stones, 25)
 }
 
 #[aoc(day11, part2)]
-fn part2(stones : &Stones) -> usize {
-    solve2(stones, 75)
+fn part2(stones : &Stones) -> u64 {
+    let mut blink = Blinker::default();
+    blink.solve(stones, 75)
 }
 
 
@@ -171,17 +68,18 @@ mod tests {
 
     #[test]
     fn sample1() {
-        assert_eq!(solve(&input_generator(SAMPLE), 6), 22);
+        let mut blink = Blinker::default();
+        assert_eq!(blink.solve(&input_generator(SAMPLE), 5), 13);
+        assert_eq!(blink.solve(&input_generator(SAMPLE), 6), 22);
         assert_eq!(part1(&input_generator(SAMPLE)), 55312);
-        // assert_eq!(part2(&input_generator(SAMPLE)), 1928);
+        assert_eq!(part2(&input_generator(SAMPLE)), 65601038650482);
     }
 
 
     #[test]
     fn sample2() {
-        assert_eq!(solve2(&input_generator(SAMPLE), 6), 22);
-        assert_eq!(solve2(&input_generator(SAMPLE), 25), 55312);
-        // assert_eq!(part2(&input_generator(SAMPLE)), 1928);
+        let mut blink = Blinker::default();
+        assert_eq!(blink.solve(&input_generator(SAMPLE), 75), 65601038650482);
     }
 
 }
